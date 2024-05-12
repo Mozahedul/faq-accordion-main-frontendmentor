1658ecfa936fe2f52597b23dab169ad8a9e8b9ae
Mozahedul <mozahed001@gmail.com>
2024-05-12 12:47:53 +0600

    Add JavaScript code for accordion

---
+++ accordion.js
@@ -0,0 +1,23 @@
+const accordionBtns = document.getElementsByClassName("accordion-btn");
+const details = document.getElementsByClassName("details");
+const expandIcons = document.getElementsByClassName("expand-icon");
+console.log("expandIcons", expandIcons);
+
+for (let i = 0; i < accordionBtns.length; i++) {
+  accordionBtns[i].addEventListener("click", function () {
+    if (details[i].offsetHeight !== details[i].scrollHeight) {
+      details[i].style.height = details[i].scrollHeight + "px";
+      expandIcons[i].src = "./assets/images/icon-minus.svg";
+    } else {
+      details[i].style.height = 0 + "px";
+      expandIcons[i].src = "./assets/images/icon-plus.svg";
+    }
+
+    for (let j = 0; j < details.length; j++) {
+      if (i !== j) {
+        details[j].style.height = 0 + "px";
+        expandIcons[j].src = "./assets/images/icon-plus.svg";
+      }
+    }
+  });
+}
